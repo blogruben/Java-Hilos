3c720540439ff282c5bd8207ad01dab2b26869c3
Ruben-R-C <Info@blogruben.com>
2023-02-04 20:52:40 +0100

    Add pintar_log_con_logBack_filtrando_por_marker

--- pintar_log_con_logBack_filtrando_por_marker/src/main/java/org/ruben/java/logback/logsFiltros/FiltroLogSecundario.java
+++ pintar_log_con_logBack_filtrando_por_marker/src/main/java/org/ruben/java/logback/logsFiltros/FiltroLogSecundario.java
@@ -1,16 +1,14 @@
-package org.ruben.java.logback;
+package org.ruben.java.logback.logsFiltros;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
-public class FiltroLog extends Filter<ILoggingEvent> {
+public class FiltroLogSecundario extends Filter<ILoggingEvent> {
 
   @Override
   public FilterReply decide(ILoggingEvent event) {    
-    
-    if (event.getMessage().contains("Ruben")) {
-      System.out.println(event.getMarker());
+    if (event.getMarker().toString().equals("secundario")) {
       return FilterReply.ACCEPT;
     } else {
       return FilterReply.DENY;
